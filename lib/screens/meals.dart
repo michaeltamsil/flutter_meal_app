49b115a4129c428a725d85c9d7a33c8d7f9aac2b
Michael Tamsil <michaeltamsil@gmaill.com>
2024-02-05 09:55:02 +0700

    162. Improving the MealItem Widget

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_meal_app/models/meal.dart';
+import 'package:flutter_meal_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -36,8 +37,11 @@ class MealsScreen extends StatelessWidget {
     );
     if (meals.isNotEmpty) {
       content = ListView.builder(
-          itemBuilder: (ctx, index) => Text(meals[index].title),
-          itemCount: meals.length);
+        itemBuilder: (ctx, index) => MealItem(
+          meal: meals[index],
+        ),
+        itemCount: meals.length,
+      );
     }
 
     return Scaffold(
---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+import 'package:flutter_meal_app/models/meal.dart';
+import 'package:flutter_meal_app/widgets/meal_item_trait.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meal});
+
+  final Meal meal;
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        clipBehavior: Clip.hardEdge,
+        elevation: 8,
+        margin: const EdgeInsets.all(18),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(28),
+        ),
+        child: InkWell(
+          onTap: () {},
+          child: Stack(
+            children: [
+              FadeInImage(
+                fit: BoxFit.cover,
+                height: 200,
+                image: NetworkImage(meal.imageUrl),
+                placeholder: MemoryImage(kTransparentImage),
+                width: double.infinity,
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  color: Colors.black54,
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 6,
+                    horizontal: 44,
+                  ),
+                  child: Column(children: [
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis, // Very long text ...
+                      softWrap: true,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label: '${meal.duration} min',
+                        ),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.work,
+                          label: complexityText,
+                        ),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.attach_money,
+                          label: affordabilityText,
+                        )
+                      ],
+                    ),
+                  ]),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
