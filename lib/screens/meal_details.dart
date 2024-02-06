e4fccd141cae7605babe16da9ff5341289970ef3
Michael Tamsil <michaeltamsil@gmaill.com>
2024-02-06 10:34:41 +0700

    164. Improving the MealDetails Screen

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -12,17 +12,66 @@ class MealDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          meal.title,
+        appBar: AppBar(
+          title: Text(
+            meal.title,
+          ),
         ),
-      ),
-      body: Image.network(
-        fit: BoxFit.cover,
-        height: 300,
-        meal.imageUrl,
-        width: double.infinity,
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            // SingleChildScroolView
+            children: [
+              Image.network(
+                fit: BoxFit.cover,
+                height: 300,
+                meal.imageUrl,
+                width: double.infinity,
+              ),
+              const SizedBox(height: 14),
+              Text(
+                'Ingredients',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              for (final ingredient in meal.ingredients)
+                Text(
+                  ingredient,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                ),
+              const SizedBox(height: 24),
+              Text(
+                'Steps',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              for (final step in meal.steps)
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 8,
+                  ),
+                  child: Text(
+                    step,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+            ],
+          ),
+        ));
   }
 }
